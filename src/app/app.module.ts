107aee4a2323c33673e3d73f70799968f8919432
mswift42 <martin.haesler@gmail.com>
2018-01-16 17:43:18 +0000

    include searchinputcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchInputComponentComponent } from './search-input-component/search-input-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchInputComponentComponent
   ],
   imports: [
     BrowserModule
