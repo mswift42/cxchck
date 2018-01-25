6b54643111c000255e4849e5b538023b4fed6f1a
mswift42 <martin.haesler@gmail.com>
2018-01-25 16:55:25 +0000

    reduce image size.

--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { MockProductService } from '../mock-product.service';
 
 @Component({
   selector: 'app-result-list',
   templateUrl: './result-list.component.html',
-  styleUrls: ['./result-list.component.css']
+  styleUrls: ['./result-list.component.css'],
+  providers: [MockProductService]
 })
 export class ResultListComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private mockProductService: MockProductService) { }
 
   ngOnInit() {
+    this.products = this.mockProductService.mockProducts;
   }
 
 }
