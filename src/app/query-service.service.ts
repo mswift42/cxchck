4aad5a9235411860f1777b2342cc553cd3b11e95
mswift42 <martin.haesler@gmail.com>
2018-01-27 22:34:55 +0000

    Add url method.

--- src/app/query-service.service.ts
+++ src/app/query-service.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class QueryServiceService {
+  private _products: string[] = [];
+  private url(query: string, location: string): string {
+    return 'http://localhost:4242/querycx?query=' + query +
+      '$location=' + location;
+  }
+
+  get products(): string[] {
+    return this._products
+  }
 
   constructor() { }
 
