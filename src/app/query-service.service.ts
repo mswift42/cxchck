633ecafad17614bb52d499f51425e5d33f0b0972
mswift42 <martin.haesler@gmail.com>
2018-01-29 14:23:58 +0000

    implement http service to retrieve products from go app.

--- src/app/query-service.service.ts
+++ src/app/query-service.service.ts
@@ -2,17 +2,19 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Product} from './product';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 
 @Injectable()
 export class QueryServiceService {
   private url(query: string, location: string): string {
     return 'http://localhost:4242/querycx?query=' + query +
-      '$location=' + location;
+      '&location=' + location;
   }
 
-  getProducts(query: string, location: string): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url(query, location));
+  getProducts(query: string, location: string): Observable<any> {
+    const u = this.url(query, location);
+    return  this.http.get(u)
+      .map((data: any) => data);
   }
 
 
--- src/app/search-input-component/search-input-component.component.ts
+++ src/app/search-input-component/search-input-component.component.ts
@@ -1,12 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {MockProductService} from '../mock-product.service';
 import {Product} from '../product';
+import {QueryServiceService} from '../query-service.service';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
+import {from} from 'rxjs/observable/from';
+
 
 @Component({
   selector: 'app-search-input-component',
   templateUrl: './search-input-component.component.html',
   styleUrls: ['./search-input-component.component.css'],
-  providers: [MockProductService]
+  providers: [QueryServiceService]
 })
 export class SearchInputComponentComponent implements OnInit {
   value = '';
@@ -18,17 +23,29 @@ export class SearchInputComponentComponent implements OnInit {
       'Cameron Toll', '3017'
     )
   ];
-  products: Product[];
+  products: Product[] = [];
 
   activeStore = this.storeList[0];
 
-
-  searchProduct(inp) {
-    console.log(inp.target.value);
+  setProducts(response: Response) {
   }
 
-  constructor(private mockProductService: MockProductService) {
-    this.products = this.mockProductService.mockProducts;
+  searchProduct(inp) {
+    this.products = [];
+    this.queryService.getProducts(
+      inp.target.value, this.activeStore.location
+    ).subscribe(
+      (data: any) => data.forEach((i) => this.products.push(
+        new Product(i['title'],
+          'https://uk.webuy.com' + i['thumbnail'],
+          i['price'],
+          '',
+          i['url'])
+      )));
+
+  }
+
+  constructor(private queryService: QueryServiceService) {
   }
 
   ngOnInit() {
