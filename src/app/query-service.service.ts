b41852a7d593699e007bbc941574b1c97dd1ded0
mswift42 <martin.haesler@gmail.com>
2018-01-30 12:51:37 +0000

    Replace space between search terms with ,

--- src/app/query-service.service.ts
+++ src/app/query-service.service.ts
@@ -3,18 +3,22 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Product} from './product';
 import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class QueryServiceService {
   private url(query: string, location: string): string {
-    return 'http://localhost:4242/querycx?query=' + query +
+    const trimmed = query.trim().replace( ' ', ',');
+    return 'http://localhost:4242/querycx?query=' + trimmed +
       '&location=' + location;
   }
 
   getProducts(query: string, location: string): Observable<any> {
     const u = this.url(query, location);
     return  this.http.get(u)
-      .map((data: any) => data);
+      .map((data: any) => data)
+      .catch((err) => Observable.throw(err));
   }
 
 
