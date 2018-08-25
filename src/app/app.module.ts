c9abdeaf10e94e5048ab2b64b31c51c81fa55022
mswift42 <martin.haesler@gmail.com>
2018-08-25 13:20:37 +0100

    include cxsearch component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CxsearchComponent } from './cxsearch/cxsearch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CxsearchComponent
   ],
   imports: [
     BrowserModule
