17c80cb50ee522475d097271cdc1abfe1e157af4
mswift42 <martin.haesler@gmail.com>
2018-08-25 13:46:48 +0100

    add dep for material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CxsearchComponent } from './cxsearch/cxsearch.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CxsearchComponent } from './cxsearch/cxsearch.component';
     CxsearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
