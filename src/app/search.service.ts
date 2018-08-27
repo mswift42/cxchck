2e10dc34381bcc9af32e0c7b7a5af5a1be48d596
mswift42 <martin.haesler@gmail.com>
2018-08-27 13:20:44 +0100

    add method url.

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private url(query: string, location: string): string {
+    return `http://localhost:4242/querycx?query=${query}&location=${location}`;
+  }
 
   constructor() { }
 }
